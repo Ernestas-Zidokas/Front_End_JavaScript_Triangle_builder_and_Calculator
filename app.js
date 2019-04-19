6bf3fd951543e2c08dc5ba94de469b86c67e0a3e
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-19 17:57:31 +0300

    isParagram uzduotis padaryta

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
-var string = "The quick brown fox jumps over the lazy."
-
-let alphabet =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var string = "THE quick brown fox jumps over the lazy dog."
 
 function isPangram(string){
+    let alphabet =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let lowerString = string.toLowerCase();
     let correct = true;
     alphabet.forEach((char)=> {
-        if(!string.includes(char)){
-            console.log('false', correct);
+        if(!lowerString.includes(char)){
             correct = false;
-            console.log(char);
         }
     })
-    console.log('viskas ok', correct);
     return correct;
 }
 
 isPangram(string);
-
-document.querySelector('#submit').addEventListener('click', event => {
-    event.preventDefault();
-    console.log('user pressed submit');
-    }
-)
-
-document.querySelector('body').addEventListener('keydown', event => {
-    event.preventDefault();
-
-    if(event.keyCode === 39){
-        console.log('paspaudei ->');
-    }
-});
-
-document.querySelector('#submit').addEventListener('mouseover', event => {
-    document.querySelector('body').classList.add("green");
-});
-
-document.querySelector('body').addEventListener('wheel', event => {
-    console.log(event);
-    document.querySelector('body').insertAdjacentHTML('beforeend', '<div>Scrolinaaaaam</div>')
-})
