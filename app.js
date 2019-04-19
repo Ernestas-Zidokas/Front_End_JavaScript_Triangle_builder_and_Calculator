640261e4ae08c89baec6b5d6eee4de692c5d0167
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-19 17:39:47 +0300

    JS querySelectors + CodeWars uzduotis(neveikia)

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-let alausKiekis = Math.floor(Math.random() * 5);
-let alausBokalai = Math.floor(Math.random() * 5).toString();
+var string = "The quick brown fox jumps over the lazy."
 
-if (alausKiekis == alausBokalai) {
-    document.write('Isgeriau 5 alaus')
-} else if (alausKiekis > alausBokalai) {
-    document.write(`Isgeriau ${alausKiekis} o skarbos kazkur dingo`)
-} else {
-    document.write(`dar liko: ${(alausBokalai - Number(alausKiekis)).toString()} alaus`)
+let alphabet =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+function isPangram(string){
+    let correct = true;
+    alphabet.forEach((char)=> {
+        if(!string.includes(char)){
+            console.log('false', correct);
+            correct = false;
+            console.log(char);
+        }
+    })
+    console.log('viskas ok', correct);
+    return correct;
 }
 
-// let alus = alausKiekis === alausBokalai;
-// if(alus) {
-//     document.write('Laimejau')
-// } else {
-//     document.write('Nepasiseke')
-// }
+isPangram(string);
+
+document.querySelector('#submit').addEventListener('click', event => {
+    event.preventDefault();
+    console.log('user pressed submit');
+    }
+)
+
+document.querySelector('body').addEventListener('keydown', event => {
+    event.preventDefault();
+
+    if(event.keyCode === 39){
+        console.log('paspaudei ->');
+    }
+});
+
+document.querySelector('#submit').addEventListener('mouseover', event => {
+    document.querySelector('body').classList.add("green");
+});
+
+document.querySelector('body').addEventListener('wheel', event => {
+    console.log(event);
+    document.querySelector('body').insertAdjacentHTML('beforeend', '<div>Scrolinaaaaam</div>')
+})
