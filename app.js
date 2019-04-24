40ad3c806a9f86e1d612fb3bd076dd42138ed9ef
Katamaranas <ernestas.zidokas@gmail.com>
2019-04-24 20:33:43 +0300

    Build a Triangle, Pyramid or Romb

--- app.js
+++ app.js
@@ -1,83 +1,92 @@
-function sum(num1, num2) {
-    return num1 + num2;
-}
+function triangle(number) {
+  let triangle = '';
+  let star = '*';
+  let space = "&nbsp;";
 
-function sub(num1, num2) {
-    return num1 - num2;
-}
-
-function div(num1, num2) {
-    return num1 / num2;
-}
-const mul = (num1, num2) => num1 * num2;
-const mod = (num1, num2) => num1 % num2;
-
-// document.querySelector("button[name='sum']").addEventListener('click', (event)=>{
-//     let number1 = Number(document.querySelector("input[name='number1']").value);
-//     let number2 = Number(document.querySelector("input[name='number2']").value);
-//     document.querySelector('#result').textContent = sum(number1, number2);
-// })
-
-// document.querySelector("button[name='sub']").addEventListener('click', (event)=>{
-//     let number1 = Number(document.querySelector("input[name='number1']").value);
-//     let number2 = Number(document.querySelector("input[name='number2']").value);
-//     document.querySelector('#result').textContent = sub(number1, number2);
-// })
-//
-// document.querySelector("button[name='div']").addEventListener('click', (event)=>{
-//     let number1 = Number(document.querySelector("input[name='number1']").value);
-//     let number2 = Number(document.querySelector("input[name='number2']").value);
-//     document.querySelector('#result').textContent = div(number1, number2);
-// })
-//
-// document.querySelector("button[name='mul']").addEventListener('click', (event)=>{
-//     let number1 = Number(document.querySelector("input[name='number1']").value);
-//     let number2 = Number(document.querySelector("input[name='number2']").value);
-//     document.querySelector('#result').textContent = mul(number1, number2);
-// })
-//
-// document.querySelector("button[name='mod']").addEventListener('click', (event)=>{
-//     let number1 = Number(document.querySelector("input[name='number1']").value);
-//     let number2 = Number(document.querySelector("input[name='number2']").value);
-//     document.querySelector('#result').textContent = mod(number1, number2);
-// })
-
-// 1. funkcija gauna user inputa
-// 2. buttons divui deti event listeneri ir jo viduj isitikinti kad buvo paspaustas jo mygtukas ir is event objekto issiaiskinti kuria funckija kviesti
-
-const userInput = (selector)=>{
-    return Number(document.querySelector(selector).value);
-}
-
-document.querySelector('#buttons').addEventListener('click',(event)=>{
-    let buttonName = event.target.name;
-    let result = '';
-
-    if(event.target.tagName == 'BUTTON'){
-        switch (buttonName) {
-            case 'sum':
-            result = sum(userInput("input[name='number1']"), userInput("input[name='number2']"));
-            break;
-            case 'div':
-            result = div(userInput("input[name='number1']"), userInput("input[name='number2']"));
-            break;
-            case 'sub':
-            result = sub(userInput("input[name='number1']"), userInput("input[name='number2']"));
-            break;
-            case 'mul':
-            result = mul(userInput("input[name='number1']"), userInput("input[name='number2']"));
-            break;
-            case 'mod':
-            result = mod(userInput("input[name='number1']"), userInput("input[name='number2']"));
-            break;
-            case 'clear':
-            document.querySelector("input[name='number1']").value = '';
-            document.querySelector("input[name='number2']").value = '';
-            document.querySelector("#result").textContent = '';
-            break;
-            default:
-            break;
-        }
-        document.querySelector('#result').textContent = result;
+  for (let i = 0; i < number; i++) {
+    for (let j = 0; j <= i; j++) {
+      triangle += star + space;
     }
+    triangle +='<br>';
+  }
+  document.querySelector('#output').innerHTML = triangle;
+}
+
+document.querySelector('#buttons').addEventListener('click', (event)=>{
+  let number = Number(document.querySelector('#number').value);
+  let pyramidType = document.querySelector('#pyramidType').value;
+  let id = event.target.id;
+
+  switch (id) {
+    case 'triangle':
+    triangle(number);
+    break;
+    case 'pyramid':
+    pyramid(number, pyramidType);
+    break;
+    case 'romb':
+    romb(number);
+    break;
+    default:
+    break;
+  }
 })
+
+function pyramid(number, reversed) {
+  let pyramid = '';
+  let star = '*';
+  let space = "&nbsp;";
+
+  for (let i = 0; i < number; i++) {
+    if(reversed == 'true'){
+      for (let k = 0; k < i; k++) {
+        pyramid += space;
+      }
+
+      for (let j = 0; j < (number - i); j++) {
+        pyramid += star + space;
+      }
+    } else {
+      for (let j = 0; j < (number - i); j++) {
+        pyramid += space;
+      }
+
+      for (let k = 0; k < i; k++) {
+        pyramid += star + space;
+      }
+    }
+    pyramid +='<br>';
+  }
+  document.querySelector('#output').innerHTML = pyramid;
+}
+
+function romb(number) {
+  let pyramid = '';
+  let star = '*';
+  let space = "&nbsp;";
+
+  for (let i = 0; i < number; i++) {
+    let halfNumber = number/2;
+
+    if(i <= halfNumber){
+      for (let j = 0; j < (number - i); j++) {
+        pyramid += space;
+      }
+
+      for (let k = 0; k < i; k++) {
+        pyramid += star + space;
+      }
+    } else {
+      for (let m = 0; m < i; m++) {
+        pyramid += space;
+      }
+
+      for (let l = 0; l < (number - i); l++) {
+        pyramid += star + space;
+      }
+    }
+
+    pyramid +='<br>';
+  }
+  document.querySelector('#output').innerHTML = pyramid;
+}
